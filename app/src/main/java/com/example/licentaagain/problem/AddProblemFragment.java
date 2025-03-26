14a437cce0d6965818aca8a0f90d3b619569f502
ingridesuper <ingrid.paslaru@gmail.com>
2025-03-26 11:00:41 +0200

    Added Places API - Search Location

--- app/src/main/java/com/example/licentaagain/problem/AddProblemFragment.java
+++ app/src/main/java/com/example/licentaagain/problem/AddProblemFragment.java
@@ -1,66 +1,92 @@
 package com.example.licentaagain.problem;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.licentaagain.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddProblemFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddProblemFragment extends Fragment {
+import java.util.Arrays;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AddProblemFragment extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private GoogleMap myMap;
+    private SupportMapFragment mapFragment;
 
     public AddProblemFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddProblemFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddProblemFragment newInstance(String param1, String param2) {
-        AddProblemFragment fragment = new AddProblemFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        // Initialize Google Places API (Add your API key here)
+        if (!Places.isInitialized()) {
+            Places.initialize(requireContext(), "AIzaSyBbOOjwR9Eq3CGJnGZhg9fMssUBRFlMDpc");
         }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_problem, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize the Map
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        // Initialize AutocompleteSupportFragment
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.DISPLAY_NAME, Place.Field.LOCATION));
+
+        // Handle the place selection
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                LatLng latLng = place.getLocation(;
+                if (latLng != null) {
+                    myMap.clear();
+                    myMap.addMarker(new MarkerOptions().position(latLng).title(place.getName()));
+                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    Log.i("Selected Place", "Place: " + place.getDisplayName() + ", LatLng: " + latLng);
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Log.e("Autocomplete Error", "Error: " + status.getStatusMessage());
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+        Log.i("AddProblemFragment", "Map is ready");
     }
 }
