0565bbfe56552fa248657b479f65f4a6bc56f292
ingridesuper <ingrid.paslaru@gmail.com>
2025-04-14 16:20:02 +0300

    Added signing buttons to problem details page

--- app/src/main/java/com/example/licentaagain/problem/ProblemDetailsFragment.java
+++ app/src/main/java/com/example/licentaagain/problem/ProblemDetailsFragment.java
@@ -5,24 +5,38 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.licentaagain.R;
 import com.example.licentaagain.custom_adapters.ImageAdapterProblemDetails;
+import com.example.licentaagain.mainpage.MainPageFragment;
 import com.example.licentaagain.models.Problem;
+import com.example.licentaagain.models.ProblemSignature;
 import com.example.licentaagain.repositories.UserRepository;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 
 public class ProblemDetailsFragment extends Fragment {
     private Problem problem;
+    private Button btnClose;
+    private Button btnSign;
+    private Button btnSigned;
 
 
     public ProblemDetailsFragment() {
@@ -50,17 +64,109 @@ public class ProblemDetailsFragment extends Fragment {
         }
 
         fillUiWithProblemData(view);
+        subscribeButtonsToEvents();
+    }
+
+    private void subscribeButtonsToEvents() {
+        subscribeBtnCloseToEvent();
+        subscribeSigningButtonsToEvents();
+    }
+
+    private void subscribeSigningButtonsToEvents() {
+        btnSign.setOnClickListener(v ->
+                addSignature(problem, success -> updateButtonVisibility(true))
+        );
+
+        btnSigned.setOnClickListener(v ->
+                removeSignature(problem, success -> updateButtonVisibility(false))
+        );
+    }
+
+    private void addSignature(Problem problem, Consumer<Boolean> callback) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        new UserRepository().checkIfUserHasNameSurnameSectorData(FirebaseAuth.getInstance().getCurrentUser().getUid(), result->{
+            if(result){
+                ProblemSignature newSignature=new ProblemSignature(problem.getId(), FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                db.collection("problem_signatures")
+                        .add(newSignature)
+                        .addOnSuccessListener(documentReference -> {
+                            Log.i("Firestore", "Semnatura adaugata: " + documentReference.getId());
+                            callback.accept(true);
+                        })
+                        .addOnFailureListener(e -> {
+                            Log.e("Firestore", "Eroare la adaugarea semnaturii", e);
+                            callback.accept(false);
+                        });
+            }
+            else{
+                Toast.makeText(getContext(), "Completati-va profilul pentru a putea semna!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    private void removeSignature(Problem problem, Consumer<Boolean> callback){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("problem_signatures")
+                .whereEqualTo("userId", FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .whereEqualTo("problemId", problem.getId())
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if (!queryDocumentSnapshots.isEmpty()) {
+                        for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                            db.collection("problem_signatures").document(doc.getId()).delete()
+                                    .addOnSuccessListener(aVoid -> {
+                                        Log.i("Firestore", "Semnatura eliminata");
+                                        callback.accept(true);
+                                    })
+                                    .addOnFailureListener(e -> {
+                                        Log.e("Firestore", "Eroare la stergerea semnaturii", e);
+                                        callback.accept(false);
+                                    });
+                        }
+                    } else {
+                        callback.accept(false);
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("Firestore", "Eroare la cautarea semnaturii", e);
+                    callback.accept(false);
+                });
+    }
+
+    private void subscribeBtnCloseToEvent() {
+        btnClose.setOnClickListener(v->{
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+
+            if (fragmentManager.getBackStackEntryCount() > 0) {
+                fragmentManager.popBackStack();  // revine la fragmentul anterior fara a crea unul nou
+                //cred ca aucu mai bn te uiti la cum il pornesti si alegi una (mai tarziu)
+            } else {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_view, new MainPageFragment())
+                        .commit();
+            }
+            BottomNavigationView bottomNavigationView = requireActivity().findViewById(R.id.bottom_navigation);
+            bottomNavigationView.setSelectedItemId(R.id.bmHome);
+        });
     }
 
     private void fillUiWithProblemData(@NonNull View view) {
-        TextView tvHelloProblem= view.findViewById(R.id.tvProblemTitle);
+        TextView tvProblemTitle= view.findViewById(R.id.tvProblemTitle);
         TextView tvProblemAuthor=view.findViewById(R.id.tvProblemAuthor);
         TextView tvProblemDescription=view.findViewById(R.id.tvProblemDescription);
         TextView tvProblemCategory=view.findViewById(R.id.tvProblemCategory);
         TextView tvProblemAddressSector=view.findViewById(R.id.tvProblemAddressSector);
         RecyclerView recyclerViewPictures=view.findViewById(R.id.recyclerViewPictures);
+        btnClose=view.findViewById(R.id.btnClose);
+        btnSign=view.findViewById(R.id.btnSign);
+        btnSigned=view.findViewById(R.id.btnSigned);
 
-        tvHelloProblem.setText(problem.getTitle());
+        problemSignedByUser(problem, isSigned -> {
+            updateButtonVisibility(isSigned);
+        });
+
+        tvProblemTitle.setText(problem.getTitle());
         new UserRepository().getUserNameSurnameBasedOnId(problem.getAuthorUid(), fullName->{
             tvProblemAuthor.setText("Reported by: "+fullName);
         });
@@ -71,5 +177,33 @@ public class ProblemDetailsFragment extends Fragment {
         List<String> problemImageUrls=problem.getImageUrls();
         ImageAdapterProblemDetails adapter=new ImageAdapterProblemDetails(getContext(), problemImageUrls);
         recyclerViewPictures.setAdapter(adapter);
+    }
+
+    private void updateButtonVisibility(Boolean isSigned) {
+        btnSign.setVisibility(isSigned ? View.INVISIBLE : View.VISIBLE);
+        btnSigned.setVisibility(isSigned ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    private void problemSignedByUser(Problem problem, Consumer<Boolean> callback) {
+        String currentUid= FirebaseAuth.getInstance().getCurrentUser().getUid();
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("problem_signatures")
+                .whereEqualTo("userId", currentUid)
+                .whereEqualTo("problemId", problem.getId())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        if(task.getResult().isEmpty()){
+                            callback.accept(false);
+                        }
+                        else {
+                            callback.accept(true);
+                        }
+                    }
+                    else {
+                        Log.e("semnaturi", "eroare");
+                        callback.accept(false);
+                    }
+                });
     }
 }
